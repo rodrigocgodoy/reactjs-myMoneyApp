78353c67cfd2a176dd0f31bd3375fb9b60f99c6a
Rodrigo Godoy <rodrigocgodoy@hotmail.com>
2019-08-08 13:43:23 -0300

    primeiro commit

---
+++ src/common/template/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            Copyright &copy; 2019 
+            <a href="https://www.linkedin.com/in/rodrigo-correia-godoy/">
+                Rodrigo Godoy
+            </a>.
+        </strong>
+    </footer>
+)
